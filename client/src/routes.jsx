d56afd45535c8dd3d1efcdb96fac697d0757dfd5
ali <alikhodaveerdi@yahoo.com>
2023-08-06 15:08:24 +0330

    baseadmin

---
+++ client/src/routes.jsx
@@ -0,0 +1,31 @@
+import {
+  HomeIcon,
+  UserCircleIcon,
+  TableCellsIcon,
+  BellIcon,
+  ArrowRightOnRectangleIcon,
+  UserPlusIcon,
+} from "@heroicons/react/24/solid";
+import { Home, Profile, Tables, Notifications } from "@/pages/dashboard";
+import { SignIn, SignUp } from "@/pages/auth";
+
+const icon = {
+  className: "w-5 h-5 text-inherit",
+};
+
+export const routes = [
+  {
+    layout: "dashboard",
+    pages: [
+      {
+        icon: <HomeIcon {...icon} />,
+        name: "داشبورد",
+        path: "/home",
+        element: <Home />,
+      },
+    ],
+  },
+
+];
+
+export default routes;
